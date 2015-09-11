b4a80e7a386f100145874b6d16dc4e72e3a0498c
BogDan Vatra <bogdan@kde.org>
2015-09-11 12:08:45 +0300

    add some copyright headers

--- fuse/ui/qml/spectrumscreen.h
+++ fuse/ui/qml/spectrumscreen.h
@@ -1,5 +1,22 @@
-#ifndef SNAPSHOTSCREEN_H
-#define SNAPSHOTSCREEN_H
+/*
+    Copyright (c) 2015, BogDan Vatra <bogdan@kde.org>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SPECTRUMSHOTSCREEN_H
+#define SPECTRUMSHOTSCREEN_H
 
 #include <QImage>
 #include <QQuickImageProvider>
@@ -20,4 +37,4 @@ public:
     QImage requestImage(const QString &id, QSize *size, const QSize &requestedSize);
 };
 
-#endif // SNAPSHOTSCREEN_H
+#endif // SPECTRUMSHOTSCREEN_H
