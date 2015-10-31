fd986f3f9c6871818d5d9a0c383935d7c69dd9f7
BogDan Vatra <bogdan@kde.org>
2015-10-31 10:51:44 +0200

    exit event loop when hitting a breakpoint.

--- fuse/z80/z80_ops.c
+++ fuse/z80/z80_ops.c
@@ -157,8 +157,10 @@ z80_do_opcodes( void )
     /* Check if the debugger should become active at this point */
     CHECK( debugger, debugger_mode != DEBUGGER_MODE_INACTIVE )
 
-    if( !z80.halted && debugger_check( DEBUGGER_BREAKPOINT_TYPE_EXECUTE, PC ) )
+    if( !z80.halted && debugger_check( DEBUGGER_BREAKPOINT_TYPE_EXECUTE, PC ) ) {
       debugger_trap();
+      break;
+    }
 
     END_CHECK
 
