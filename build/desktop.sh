e8dde4b37193a89a5fe39ebd853eaf11aaab4951
BogDan Vatra <bogdan@kde.org>
2018-04-22 09:49:14 +0300

    Use different config file on linux

--- build/desktop.sh
+++ build/desktop.sh
@@ -46,6 +46,7 @@ mkdir -p $BUILD_DIR/libspectrum
 
 JOBS="-j$(nproc)"
 
+export CFLAGS="${CFLAGS} -Ofast -DCONFIG_FILE_NAME=\\\".spectacolrc\\\""
 pushd $BUILD_DIR/libspectrum
   if [ ! -f $SRC_DIR/libspectrum/configure ]; then
     pushd $SRC_DIR/libspectrum
